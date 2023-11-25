ea0c19202b110f02d965ef9a8797c1cd2595edcf
Savitha M <savitha@Savithas-MacBook-Air.local>
2023-11-26 03:33:16 +0530

    error handling for unexpectes situation using try catch, next(err) or using a class throw new ErrorExpress(404,nonfound) middle ware default error handler

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000;
 const path=require("path");
 const Chat=require("./models/chat.js")
+const ExpressError = require('./expresserror.js');
 const { v4: uuidv4 } = require('uuid');
 var methodOverride = require('method-override')
 app.use(express.static(path.join(__dirname,"public")));
@@ -13,6 +14,7 @@ app.use(express.json());
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 const mongoose = require('mongoose');
+
 main().then((res)=>{
     console.log("mongoose connection sucess")
 })
@@ -22,63 +24,93 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/telegram');
 
 }
-// insert 
-// let chat1=new Chat({
-//   from:"savitha",
-//   to:"sandeep",
-//   msg:"hi can you help me san ",
-//   created_at: new Date()
-// });
-// chat1.save().then((res)=>{console.log(res)})
-
-app.get('/chat', async (req, res) => {
-  // from the database extract data
-  let chatt = await Chat.find();
-  res.render("app.ejs", {chatt}); // Pass the variable as an object property
-});
-// create route
-app.get('/chat/new', async (req, res) => {
-  res.render("new.ejs"); // Pass the variable as an object property
+app.get("/", (req, res) => {
+  res.send("working");
 });
 
-app.post('/chat', async (req, res) => {
-   let {from,to,msg}=req.body;
-   let newChat= new Chat({
-    from:from,
-    to:to,
-    msg:msg,
-    created_at:new Date()
-   });
-   newChat.save().then(()=>{console.log("chat is saved")})
-   res.redirect("/chat");
+app.get('/chat', async (req, res, next) => {
+  try {
+    let chatt = await Chat.find();
+    res.render("app.ejs", { chatt });
+  } catch (err) {
+    next(err);
+  }
 });
 
-
-// edit 
-app.get('/chat/:id/edit', async (req, res) => {
-  let {id}=req.params;
-  let chatid= await Chat.findById(id);
-  res.render("edit.ejs",{chatid}); // Pass the variable as an object property
-});  
-app.put('/chat/:id', async (req, res) => {
-  let {id}=req.params;
-  let {msg:newmsg}=req.body;
-  let chatid= await Chat.findByIdAndUpdate(
-    id,
-    {msg:newmsg},
-    {runValidators:true,new:true});
-    console.log(chatid)
-    res.redirect("/chat")
+app.get('/chat/new', (req, res) => {
+  res.render("new.ejs");
 });
 
-app.delete('/chat/:id', async (req, res) => {
-  let {id}=req.params;
-  let chatdEL= await Chat.findByIdAndDelete(id);
-  console.log(chatdEL);
-  res.redirect("/chat"); // Pass the variable as an object property
-});  
+app.post('/chat', async (req, res, next) => {
+  try {
+    let { from, to, msg } = req.body;
+    let newChat = new Chat({
+      from: from,
+      to: to,
+      msg: msg,
+      created_at: new Date()
+    });
 
+    await newChat.save();
+    console.log("chat is saved");
+    res.redirect("/chat");
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/chat/:id/edit', async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let chatid = await Chat.findById(id);
+    res.render("edit.ejs", { chatid });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/chat/:id', async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let chatid = await Chat.findById(id);
+    res.render("edit.ejs", { chatid });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.put('/chat/:id', async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let { msg: newmsg } = req.body;
+    let chatid = await Chat.findByIdAndUpdate(
+      id,
+      { msg: newmsg },
+      { runValidators: true, new: true }
+    );
+    console.log(chatid);
+    res.redirect("/chat");
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.delete('/chat/:id', async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let chatdEL = await Chat.findByIdAndDelete(id);
+    console.log(chatdEL);
+    res.redirect("/chat");
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  let { status = 500, message = "some error" } = err;
+  res.status(status).send(message);
+});
 
 app.listen(port, () => {
-  console.log(` express is  listening on port ${port}`)
+  console.log(`Express is listening on port ${port}`);
 });
